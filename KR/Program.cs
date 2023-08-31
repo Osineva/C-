97609066c7a366c1bab15bc03c4aec1f2805fa4c
Osineva <sinelnikova_610121@mail.ru>
2023-08-31 14:30:14 +0300

    Создание массива

--- KR/Program.cs
+++ KR/Program.cs
@@ -2,3 +2,31 @@
 //длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, 
 //либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
 //лучше обойтись исключительно массивами.
+
+
+void FillArray(int[] numbers, int minValue = 0, int maxValue = 10000)
+{
+maxValue++;
+    Random rnd = new Random();
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = rnd.Next(minValue, maxValue);
+    }
+}
+
+void Print(int[] numbers)
+{
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write($"{numbers[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int size = 3; 
+
+    int[] numbers = new int[size];
+
+    FillArray(numbers);
+
+    Print(numbers);
