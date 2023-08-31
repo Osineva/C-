ad20113a8d78bf54efc4dae30e9fa8a236d9234b
Osineva <sinelnikova_610121@mail.ru>
2023-08-31 16:15:52 +0300

    Написание кода

--- ControlWork/Program.cs
+++ ControlWork/Program.cs
@@ -1,4 +1,29 @@
 ﻿string[] inputArray = { "Russia", "Denmark", "Kazan", "1234", "1567", "-2", "computer science" };
 
+int count = 0;
+foreach (string str in inputArray)
+{
+    if (str.Length <= 3)
+    {
+        count++;
+    }
+}
 
+string[] filteredArray = new string[count];
+
+int index = 0;
+foreach (string str in inputArray)
+{
+    if (str.Length <= 3)
+    {
+        filteredArray[index] = str;
+        index++;
+    }
+}
+
+Console.WriteLine("Filtered Array:");
+foreach (string str in filteredArray)
+{
+    Console.WriteLine(str);
+}
 
